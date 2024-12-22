d8fcad3f945afefef84ef1d9e999145c02caadf3
Tejas0041 <tejaspawar70238@gmail.com>
2024-12-22 18:02:32 +0530

    [UPDATE]: Added upload method to event registration route

--- route/eventRegistration.js
+++ route/eventRegistration.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const eventRegistrationController = require('../controller/eventRegistration');
 const {isLoggedIn, isAdmin}= require("../middleware.js");
+const multer = require('multer');
+const upload = multer({ storage });
 
-router.post('/', eventRegistrationController.createRegistration);
+router.post('/', upload.single(), eventRegistrationController.createRegistration);
 
 router.get('/', eventRegistrationController.getAllRegistrations);
 router.get('/:id', eventRegistrationController.getRegistrationById);
